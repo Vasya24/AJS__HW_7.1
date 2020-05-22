55e0145c39c63201c51ca5f1d8f4bf2b86ef5fef
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-05-22 13:49:13 +0300

    Even better

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,6 +1,31 @@
 import { Character, Bowman, Swordsman, Undead, Zombie, Magician, Daemon } from '../index'
 
-const vasya = new Bowman('Базелиус');
-test('Params test', () => {
-    expect(vasya.attack).toEqual(vasya.defence)
+const v1 = new Bowman('Арбалетчик');
+test('First', () => {
+    expect(v1.attack).toEqual(v1.defence)
+});
+
+const v2 = new Swordsman('Мечник');
+test('Second', () => {
+    expect(v2.defence).toBeLessThan(30)
+});
+
+const v3 = new Undead('Скелет');
+test('Third', () => {
+    expect(v3.defence).toBeGreaterThanOrEqual(v3.attack);
+})
+
+const v4 = new Zombie('Живой труп');
+test('Fourth', () =>{
+    expect(v4.attack).toBeLessThanOrEqual(40);
+})
+
+const v5 = new Magician('Монах');
+test('Fifth', () => {
+    expect(v5.attack).not.toBe(15)
+})
+
+const v6 = new Daemon('Ифрит');
+test('Sixth', () => {
+    expect(v6.defence).not.toEqual(v6.attack)
 })
